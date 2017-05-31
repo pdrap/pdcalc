672ade606fd0ab97b56cbe9625d0b0cab8290d7d
pdrap <pdrap@pdrap.org>
2017-05-30 21:00:38 -0500

    nothing

---
+++ pdcalc/stack_element.cpp
@@ -0,0 +1,18 @@
+/*
+ * StackElement.cpp
+ *
+ *  Created on: Jun 24, 2014
+ *      Author: pdrap
+ */
+
+#include "StackElement.h"
+
+StackElement::StackElement() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StackElement::~StackElement() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ pdcalc/stack_element.h
@@ -0,0 +1,21 @@
+/*
+ * StackElement.h
+ *
+ *  Created on: Jun 24, 2014
+ *      Author: pdrap
+ */
+
+#ifndef STACKELEMENT_H_
+#define STACKELEMENT_H_
+
+class StackElement {
+
+
+public:
+	StackElement();
+	virtual ~StackElement();
+
+
+};
+
+#endif /* STACKELEMENT_H_ */
